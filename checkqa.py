87c320b2b0b85775a4f2ad140ebd642364dc02a8
Daniele Amato <71721649+Dani1809@users.noreply.github.com>
2026-02-12 16:27:25 +0100

    Update checkqa.py

--- checkqa.py
+++ checkqa.py
@@ -4,7 +4,7 @@ import torch
 import argparse
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-MODEL_ID = "Qwen/Qwen2.5-3B-Instruct"
+MODEL_ID = "Qwen/Qwen2.5-7B-Instruct"
 
 
 def load_prompt(prompt_path: str, prompt_key: str) -> str:
@@ -125,3 +125,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
